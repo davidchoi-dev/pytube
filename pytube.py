9d0ad15627e26ff1ed797a7110451eb174be1934
dangen-effy <dangen.effy@gmail.com>
2018-11-03 16:54:01 +0900

    imple white space searching, CJK support

--- pytube.py
+++ pytube.py
@@ -1,28 +1,31 @@
+#!/usr/bin/env python
+# coding=utf8
+
 import webbrowser as wb
+import urllib
 from colorama import init, Fore, Back, Style
 from termcolor import colored
 import bs4
 import os
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+from video import Video
+
 init(autoreset=True)
 
-class Video:
-    def __init__(self, title, url):
-          self.title = title
-          self.url = url
+# item limitation to fetch
+limit = 5
+
 
 videos = []
 
 # make a query
-url = 'http://www.youtube.com/results?search_query='
-print(Fore.GREEN + 'pyTube> ', end='')
+url = "http://www.youtube.com/results?search_query="
+print(Fore.GREEN + "pyTube> ", end="")
 q = input()
-url += q
-os.system( 'cls' )
-print('Browsing ...')
-# item limitation to fetch
-limit = 5
+url += urllib.parse.quote_plus(q.replace(" ", "+"))
+os.system("cls")
+print("Browsing ...")
 
 # opening up connection, grabbing the content
 uClient = uReq(url)
@@ -30,13 +33,18 @@ page_html = uClient.read()
 uClient.close()
 
 page_soup = soup(page_html, "html.parser")
-containers = page_soup.findAll("a", {"class": "yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink spf-link "})[:limit]
-os.system( 'cls' )
+containers = page_soup.findAll(
+    "a",
+    {
+        "class": "yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink spf-link "
+    },
+)[:limit]
+os.system("cls")
 
 for i, container in enumerate(containers):
     videos.append(Video(container["title"], container["href"]))
-    print(Fore.GREEN + str(i) + '.', container["title"])
+    print(Fore.GREEN + str(i) + ".", container["title"])
 
-print(Fore.GREEN + 'pyTube?> ', end='')
+print(Fore.GREEN + "pyTube?> ", end="")
 input = input()
-wb.open_new('http://www.youtube.com' + videos[int(input)].url)
+wb.open_new("http://www.youtube.com" + videos[int(input)].url)
